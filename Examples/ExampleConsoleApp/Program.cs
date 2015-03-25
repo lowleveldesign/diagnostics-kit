f0eb36a1057a3c9aa3198133f428c4719709b256
Sebastian <lowleveldesign@gmail.com>
2015-03-25 06:25:25 +0100

    dodany harvester dla log4net-a do diagnostyki

--- Examples/ExampleConsoleApp/Program.cs
+++ Examples/ExampleConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using NLog;
+﻿using log4net;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,14 @@ namespace ExampleConsoleApp
 {
     class Program
     {
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private static readonly Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly ILog logger2 = log4net.LogManager.GetLogger(typeof(Program));
 
         static void Main(string[] args) {
+            log4net.Config.XmlConfigurator.Configure();
+
             logger.Info("test");
+            logger2.Info("test-log4net");
         }
     }
 }
---
+++ LowLevelDesign.Diagnostics.Harvester.log4net/DiagnosticsKitAppender.cs
@@ -0,0 +1,56 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using LowLevelDesign.Diagnostics.Commons;
+using LowLevelDesign.Diagnostics.Commons.Connectors;
+using LowLevelDesign.Diagnostics.Commons.Models;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LowLevelDesign.Diagnostics.Harvester.log4net
+{
+    public class DiagnosticsKitAppender : AppenderSkeleton
+    {
+        public String DiagnosticsCastleUrl { get; set; }
+
+        private HttpCastleConnector connector;
+        public override void ActivateOptions() {
+            base.ActivateOptions();
+
+            connector = new HttpCastleConnector(new Uri(DiagnosticsCastleUrl));
+        }
+
+        protected override void Append(LoggingEvent loggingEvent) {
+            var process = Process.GetCurrentProcess();
+            var thread = Thread.CurrentThread;
+
+            var logrec = new LogRecord {
+                TimeUtc = DateTime.UtcNow,
+                LoggerName = loggingEvent.LoggerName,
+                LogLevel = loggingEvent.Level.Name,
+                Message = loggingEvent.RenderedMessage,
+                Server = Environment.MachineName,
+                ApplicationPath = AppDomain.CurrentDomain.BaseDirectory, // TODO: check if it's a valid approach
+                ProcessId = process.Id,
+                ProcessName = process.ProcessName,
+                ThreadId = thread.ManagedThreadId,
+                Identity = Thread.CurrentPrincipal.Identity.Name
+            };
+
+            if (loggingEvent.ExceptionObject != null) {
+                logrec.ExceptionType = loggingEvent.ExceptionObject.GetType().FullName;
+                logrec.ExceptionMessage = loggingEvent.ExceptionObject.Message;
+                logrec.ExceptionAdditionalInfo = loggingEvent.ExceptionObject.StackTrace.ShortenIfNecessary(5000);
+            }
+
+            connector.SendLogRecord(logrec);
+        }
+
+        protected override void OnClose() {
+            base.OnClose();
+
+            // dispose the connector
+            connector.Dispose();
+        }
+    }
+}
