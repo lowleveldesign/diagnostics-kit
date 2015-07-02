b1b22fd19d528a2e13651e54327a77cce96d1326
Sebastian <lowleveldesign@gmail.com>
2015-07-03 00:04:01 +0200

    log viewer - first steps

--- LowLevelDesign.Diagnostics.Castle/Models/ApplicationLogModels.cs
+++ LowLevelDesign.Diagnostics.Castle/Models/ApplicationLogModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LowLevelDesign.Diagnostics.Commons.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,25 +8,40 @@ namespace LowLevelDesign.Diagnostics.Castle.Models
 {
     public class ApplicationLogFilterModel
     {
-        public DateTime FromUtc { get; set; }
-
-        public DateTime ToUtc { get; set; }
-
-        public String Logger { get; set; }
-
-        // FIXME: public LogRecord.ELogLevel[] Levels { get; set; }
+        public String ApplicationName { get; set; }
 
         public String ApplicationPath { get; set; }
 
         public String Server { get; set; }
 
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+
+        public String Logger { get; set; }
+
+        public short LevelFrom { get; set; }
+
+        public short LevelTo {get; set; }
+
         public String Keywords { get; set; }
 
-
+        public Tuple<short, String>[] Levels = new[] {
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Trace, "Trace"),
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Debug, "Debug"),
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Info, "Info"),
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Warning, "Warning"),
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Error, "Error"),
+            new Tuple<short, String>((short)LogRecord.ELogLevel.Critical, "Critical")
+        };
     }
 
-    public class ApplicationLogSearchResult
+    public class ApplicationLogSearchResults
     {
-        // FIXME what should be in the results?
+        public LogRecord[] FoundItems { get; set; }
+
+        public int Limit { get; set; }
+
+        public int Offset { get; set; }
     }
 }
--- LowLevelDesign.Diagnostics.Castle/Modules/ApplicationLogModule.cs
+++ LowLevelDesign.Diagnostics.Castle/Modules/ApplicationLogModule.cs
@@ -1,16 +1,74 @@
 ﻿using LowLevelDesign.Diagnostics.Castle.Models;
+using LowLevelDesign.Diagnostics.Commons.Config;
+using LowLevelDesign.Diagnostics.Commons.Models;
 using LowLevelDesign.Diagnostics.Commons.Storage;
 using Nancy;
+using Nancy.ModelBinding;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LowLevelDesign.Diagnostics.Castle.Modules
 {
     public sealed class ApplicationLogModule : NancyModule
     {
-        public ApplicationLogModule(ILogStore logStore) {
-            Get["/", true] = async (x, ct) => {
-                var model = new ApplicationLogSearchResult();
+        private const int MaxLogsCount = 100;
 
-                return View["ApplicationLogs.cshtml", model];
+        public ApplicationLogModule(ILogStore logStore, IAppConfigurationManager config)
+        {
+            Get["/logs/{apppath}/{server?}", true] = async (x, ct) => {
+                var app = await config.FindAppAsync(Application.GetPathFromBase64Key((String)x.apppath));
+                DateTime dateFrom = DateTime.Now.Subtract(TimeSpan.FromHours(10));
+                DateTime dateTo = DateTime.Now;
+
+                var model = new ApplicationLogFilterModel {
+                    ApplicationPath = app.Path,
+                    ApplicationName = app.Name,
+                    LevelFrom = (short)LogRecord.ELogLevel.Info,
+                    LevelTo = (short)LogRecord.ELogLevel.Critical,
+                    DateFrom = dateFrom,
+                    DateTo = dateTo,
+                    Server = x.server
+                };
+
+                ViewBag.Logs = await FilterLogsAsync(logStore, model, 0);
+
+                return View["ApplicationLog.cshtml", model];
+            };
+            Post["/logs/{apppath}/{server?}", true] = async (x, ct) => {
+                // FIXME validation
+                var model = this.Bind<ApplicationLogFilterModel>();
+
+
+
+                return View["ApplicationLog.cshtml", model];
+            };
+        }
+
+        private static async Task<ApplicationLogSearchResults> FilterLogsAsync(ILogStore logStore, ApplicationLogFilterModel filter, int offset)
+        {
+            var levels = new List<LogRecord.ELogLevel>();
+            for (short lvl = filter.LevelFrom; lvl <= filter.LevelTo; lvl++) {
+                levels.Add((LogRecord.ELogLevel)lvl);
+            }
+
+            var searchResults = await logStore.FilterLogs(new LogSearchCriteria {
+                ApplicationPath = filter.ApplicationPath,
+                Server = filter.Server,
+                FromUtc = filter.DateFrom.ToUniversalTime(),
+                ToUtc = filter.DateTo.ToUniversalTime(),
+                Logger = filter.Logger,
+                Keywords = filter.Keywords,
+                Levels = levels.ToArray(),
+                Limit = MaxLogsCount,
+                Offset = offset
+            });
+
+            return new ApplicationLogSearchResults {
+                FoundItems = searchResults.FoundItems.ToArray(),
+                Limit = MaxLogsCount,
+                Offset = offset
             };
         }
     }
