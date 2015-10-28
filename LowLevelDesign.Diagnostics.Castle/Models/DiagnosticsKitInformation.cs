fe3bdf15e74331025735b2e984fb601c742f83d3
ssolnica <ssolnica@gmail.com>
2015-10-28 06:21:05 +0100

    added version and some renamings

---
+++ LowLevelDesign.Diagnostics.Castle/Models/DiagnosticsKitInformation.cs
@@ -0,0 +1,15 @@
+﻿namespace LowLevelDesign.Diagnostics.Castle.Models
+{
+    public class DiagnosticsKitInformation
+    {
+        public string Version { get; set; }
+
+        public string UsedLogStore { get; set; }
+
+        public string UsedAppConfigurationManager { get; set; }
+
+        public string UsedAppUserManager { get; set; }
+
+        public bool IsAuthenticationEnabled { get; set; }
+    }
+}
--- LowLevelDesign.Diagnostics.Castle/Modules/InfoModule.cs
+++ LowLevelDesign.Diagnostics.Castle/Modules/InfoModule.cs
@@ -1,12 +1,24 @@
-﻿using LowLevelDesign.Diagnostics.Commons.Models;
+﻿using LowLevelDesign.Diagnostics.Castle.Config;
+using LowLevelDesign.Diagnostics.Castle.Models;
+using LowLevelDesign.Diagnostics.Commons.Models;
 using Nancy;
 
 namespace LowLevelDesign.Diagnostics.Castle.Modules
 {
     public class InfoModule : NancyModule
     {
-        public InfoModule() {
-            Get["/version"] = _ => typeof(LogRecord).Assembly.GetName().Version.ToString();
+        public InfoModule(GlobalConfig globalSettings)
+        {
+            Get["/about"] = _ => {
+                var result = new DiagnosticsKitInformation {
+                    Version = typeof(LogRecord).Assembly.GetName().Version.ToString(),
+                    UsedLogStore = AppSettings.GetLogStore().GetType().AssemblyQualifiedName,
+                    UsedAppConfigurationManager = AppSettings.GetAppConfigurationManager().GetType().AssemblyQualifiedName,
+                    UsedAppUserManager = AppSettings.GetAppUserManager().GetType().AssemblyQualifiedName,
+                    IsAuthenticationEnabled = globalSettings.IsAuthenticationEnabled()
+                };
+                return View["About.cshtml", result];
+            };
         }
     }
 }
