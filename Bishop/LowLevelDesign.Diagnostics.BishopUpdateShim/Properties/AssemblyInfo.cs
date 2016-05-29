0c1afd1dd9d043c10a7ccebf1b70cfe2513012e3
Sebastian Solnica <ssolnica@gmail.com>
2016-05-29 08:54:26 +0200

    updated license text in c# files

--- Bishop/LowLevelDesign.Diagnostics.BishopUpdateShim/Properties/AssemblyInfo.cs
+++ Bishop/LowLevelDesign.Diagnostics.BishopUpdateShim/Properties/AssemblyInfo.cs
@@ -1,4 +1,20 @@
-﻿using System.Reflection;
+﻿/**
+ *  Part of the Diagnostics Kit
+ *
+ *  Copyright (C) 2016  Sebastian Solnica
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
--- LowLevelDesign.Diagnostics.Musketeer/Config/SimpleInjectorJobFactory.cs
+++ LowLevelDesign.Diagnostics.Musketeer/Config/SimpleInjectorJobFactory.cs
@@ -1,4 +1,20 @@
-﻿using Quartz;
+﻿/**
+ *  Part of the Diagnostics Kit
+ *
+ *  Copyright (C) 2016  Sebastian Solnica
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+using Quartz;
 using Quartz.Simpl;
 using Quartz.Spi;
 using SimpleInjector;
