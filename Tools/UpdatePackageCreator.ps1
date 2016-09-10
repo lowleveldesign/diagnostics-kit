d1c20268c00d26e01635684834880bafad653617
Sebastian Solnica <ssolnica@gmail.com>
2016-09-10 14:18:12 +0200

    fixes for build

--- Tools/UpdatePackageCreator.ps1
+++ Tools/UpdatePackageCreator.ps1
@@ -1,10 +1,11 @@
 
 param([Parameter(Mandatory=$True)][string]$ReleaseFolder,
       [Parameter(Mandatory=$True)][string]$ProductName,
-      [Parameter(Mandatory=$True)][string]$OutputFolder)
+      [Parameter(Mandatory=$True)][string]$OutputFolder,
+      [Parameter(Mandatory=$True)][string]$MainAssemblyName)
 
 $cmdpath = Split-Path $MyInvocation.MyCommand.Path
-$version = $(Get-ChildItem -Path "$ReleaseFolder" -Filter *.exe | Sort-Object -Property Name | Select-Object -First 1).VersionInfo.FileVersion
+$version = $(Get-ChildItem -Path "$ReleaseFolder" -Filter $MainAssemblyName | Sort-Object -Property Name | Select-Object -First 1).VersionInfo.FileVersion
 $outputZipPath = "$OutputFolder\$ProductName`_$version.zip"
 
 Write-Host $ProductName
