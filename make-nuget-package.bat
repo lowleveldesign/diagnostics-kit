7f39f44cdf02335f2597eaf1cf9f51fa977f840e
Sebastian Solnica <ssolnica@gmail.com>
2016-09-10 16:36:59 +0200

    added nuget

--- make-nuget-package.bat
+++ make-nuget-package.bat
@@ -1,10 +1,10 @@
-nuget pack -Build -Properties "Configuration=Release" LowLevelDesign.Diagnostics.Commons\LowLevelDesign.Diagnostics.Commons.csproj
-nuget pack -Build -Properties "Configuration=Release" LowLevelDesign.Diagnostics.Commons.net35\LowLevelDesign.Diagnostics.Commons.net35.csproj
-nuget pack -Build -Properties "Configuration=Release" LogStores\LowLevelDesign.Diagnostics.LogStore.Commons\LowLevelDesign.Diagnostics.LogStore.Commons.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet\LowLevelDesign.Diagnostics.Harvester.AspNet.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet.Mvc\LowLevelDesign.Diagnostics.Harvester.AspNet.Mvc.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet.WebAPI\LowLevelDesign.Diagnostics.Harvester.AspNet.WebAPI.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.log4net\LowLevelDesign.Diagnostics.Harvester.log4net.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.NLog\LowLevelDesign.Diagnostics.Harvester.NLog.csproj
-nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.SystemDiagnostics\LowLevelDesign.Diagnostics.Harvester.SystemDiagnostics.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" LowLevelDesign.Diagnostics.Commons\LowLevelDesign.Diagnostics.Commons.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" LowLevelDesign.Diagnostics.Commons.net35\LowLevelDesign.Diagnostics.Commons.net35.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" LogStores\LowLevelDesign.Diagnostics.LogStore.Commons\LowLevelDesign.Diagnostics.LogStore.Commons.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet\LowLevelDesign.Diagnostics.Harvester.AspNet.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet.Mvc\LowLevelDesign.Diagnostics.Harvester.AspNet.Mvc.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.AspNet.WebAPI\LowLevelDesign.Diagnostics.Harvester.AspNet.WebAPI.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.log4net\LowLevelDesign.Diagnostics.Harvester.log4net.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.NLog\LowLevelDesign.Diagnostics.Harvester.NLog.csproj
+Tools\nuget pack -Build -Properties "Configuration=Release" Harvesters\LowLevelDesign.Diagnostics.Harvester.SystemDiagnostics\LowLevelDesign.Diagnostics.Harvester.SystemDiagnostics.csproj
 pause
